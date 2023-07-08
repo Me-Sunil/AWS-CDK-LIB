1d078cf231d026da78a25e06e4a6045562fb8002
Sunil <sunil016@yahoo.com>
2023-07-08 09:25:42 +0530

    #lambda, update file to main

--- lambda/bin/lambda.ts
+++ lambda/bin/lambda.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { LambdaStack } from '../lib/lambda-stack';
+import { LambdaStack } from '../lib/main';
 
 const app = new cdk.App();
 new LambdaStack(app, 'LambdaStack');
