30811e571eea1982b2784dacd5daed0d33f9c942
Sunil <sunil016@yahoo.com>
2023-07-08 12:27:48 +0530

    chore: project created with projen

--- lambda/bin/lambda.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { LambdaStack } from '../lib/main';
-
-const app = new cdk.App();
-new LambdaStack(app, 'LambdaStack');
--- stack-with-config/bin/stack-with-config.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { StackWithConfigStack } from '../lib/main';
-
-const app = new cdk.App();
-new StackWithConfigStack(app, 'StackWithConfigStack');
